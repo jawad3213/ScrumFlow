1d2d947b5f37ba7a44118fd75850ee2bf371fd39
bachirat05 <bachiratsalma@gmail.com>
2025-12-16 22:18:51 +0100

    feat(auth): Réinitialisation de mot de passe

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import ForgotPassword from './pages/ForgotPassword.jsx'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   test: {
+    // options de configuration de vitest
+    environment: 'jsdom',
+    globals: true, // pour utiliser les fonctions expect, describe, etc. sans les importer
+    include: ['src/tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
   plugins: [react()],
+  
 })
